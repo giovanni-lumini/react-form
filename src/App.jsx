4b71dc9aecaf8e849a30a0012953760488e05c5b
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-11-18 14:00:54 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/* Creare un semplice form con un campo input per il titolo di un articolo del blog. 
+Al submit del form, mostrare la lista degli articoli inseriti, con la possibilità di cancellare ciascun articolo utilizzando un'icona. */
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
